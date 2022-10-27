5f1e8de06210ed9a885ae6fb048bee38a2516fb4
Daniel Abril <danbril1015@gmail.com>
2022-10-26 22:00:56 -0500

    creo modelo de Types

---
+++ src/models/types.models.js
@@ -0,0 +1,24 @@
+const db = require("../utils/database");
+const { DataTypes } = require("sequelize");
+
+const Types = db.define(
+  "types",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = Types;
