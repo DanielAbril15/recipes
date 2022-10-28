924fce799598c28bc884a4bdc8a66041a6ace6d5
Daniel Abril <danbril1015@gmail.com>
2022-10-28 14:38:33 -0500

    se crean relaciones

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,8 +1,28 @@
-const Users = require('./users.models')
+const Users = require("./users.models");
+const Categories = require("./categories.models");
+const Ingredients = require("./ingredients.models");
+const Instructions = require("./instructions.models");
+const RecipesIngredients = require("./recipes_ingredients.models");
+const Recipes = require("./recipes.models");
+const Types = require("./types.models");
+const UsersIngredients = require("./users_ingredients.models");
+const UsersRecipes = require("./users_recipes.models");
 
 const initModels = () => {
+  Users.hasMany(Recipes);
+  Recipes.belongsTo(Users);
 
-}
+  Users.hasMany(UsersRecipes);
+  UsersRecipes.belongsTo(Users);
 
+  Recipes.hasMany(UsersRecipes);
+  UsersRecipes.belongsTo(Recipes);
 
-module.exports = initModels
+  Users.hasMany(UsersIngredients);
+  UsersIngredients.belongsTo(Users);
+
+  Ingredients.hasMany(UsersIngredients);
+  UsersIngredients.belongsTo(Ingredients);
+};
+
+module.exports = initModels;
