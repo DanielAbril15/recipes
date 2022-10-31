4b7d6decc5cfd40f078578c26a4a7ca4f2cb90c8
Daniel Abril <danbril1015@gmail.com>
2022-10-31 16:24:29 -0500

    agrego proteccion de ruta get y post en categories

--- src/categories/categories.router.js
+++ src/categories/categories.router.js
@@ -1,10 +1,20 @@
 const router = require("express").Router();
 const categoriesServices = require("./categories.services");
+const passport = require("passport");
+const adminValidate = require("../middlewares/role.middleware");
 
 router
   .route("/")
-  .get(categoriesServices.getAllCategories)
-  .post(categoriesServices.createCategory);
+  .get(
+    passport.authenticate("jwt", { session: false }),
+    adminValidate,
+    categoriesServices.getAllCategories
+  )
+  .post(
+    passport.authenticate("jwt", { session: false }),
+    adminValidate,
+    categoriesServices.createCategory
+  );
 
 router
   .route("/:id")
